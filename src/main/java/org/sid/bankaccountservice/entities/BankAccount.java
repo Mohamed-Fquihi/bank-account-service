67b8eccb4782c7c4592148fa5067e68602fbbc8c
Mohamed Fquihi <mo.fquihi@gmail.com>
2022-10-31 19:39:26 +0000

    save bank account

--- src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
+++ src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.sid.bankaccountservice.enums.AccountType;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import java.util.Date;
 @Entity
@@ -17,7 +19,8 @@ public class BankAccount {
     @Id
     private String id;
     private Date createdAt;
-    private double balance;
+    private Double balance;
     private String currency;
+    @Enumerated(EnumType.STRING)
     private AccountType type;
 }
